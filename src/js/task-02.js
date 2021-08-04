56fa3e165f60705313eb1850125d26e489813d1d
RomanIlienko <75617889+RomanIlienko@users.noreply.github.com>
2021-08-04 22:22:07 +0300

    second task done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+
+
+
+
+const makeIngredientsList = ingredients.map(item => {
+  const ingredientsListItem = document.createElement('li')
+  ingredientsListItem.classList.add('list-item')
+  ingredientsListItem.textContent = item
+  // console.log(ingredientsListItem);
+
+  return ingredientsListItem
+})
+
+const ingredientsListRef = document.querySelector('ul')
+// console.log(ingredientsListRef);
+
+ingredientsListRef.append(...makeIngredientsList)
