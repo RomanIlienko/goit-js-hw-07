3478238bc12f6c5e9ecca8e7f61f2f82494093d4
RomanIlienko <75617889+RomanIlienko@users.noreply.github.com>
2021-08-06 10:47:55 +0300

    sixth task done

---
+++ src/js/task-06.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#validation-input')
+const inputLength = input.getAttribute('data-length')
+const inputNumLength = Number(inputLength)
+// console.log(inputNumLength);
+
+const blurOnInput = () => {
+    if (input.value.length === '') {
+        input.classList.add('invalid')
+
+        return
+    }
+
+    if (input.value.length === inputNumLength) {
+        input.classList.remove('invalid')
+        input.classList.add('valid')
+
+        return
+    }
+        input.classList.remove('valid')
+        input.classList.add('invalid')
+
+}
+
+input.addEventListener('blur', blurOnInput)
